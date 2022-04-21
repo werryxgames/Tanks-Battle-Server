edecd70b8d31f4b6fd6934edad4aaeb35aafd254
werryxgames <103959882+werryxgames@users.noreply.github.com>
2022-04-21 18:32:57 +0300

    Добавлено логирование ошибок
    Теперь если произойдёт ошибка, то сервер не остановится и запишет ошибку в лог

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from singleton import set_data
 
 
 def main():
-	config = read("config.json")
 	logger = Logger(Logger.LEVEL_DEBUG, Logger.LEVEL_DEBUG)
-	set_data(config, logger)
+	try:
+		config = read("config.json")
+		set_data(config, logger)
 
-	start_tcp_server()
+		start_tcp_server()
+	except BaseException as e:
+		logger.error(e)
 
 
 if __name__ == '__main__':
